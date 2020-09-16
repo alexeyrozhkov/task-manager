3e76071e7a174566822e41542df42658fb6b46f4
arozhkov <alex31inq@gmail.com>
2020-09-16 21:19:39 +0300

    event

--- src/js/module.js
+++ src/js/module.js
@@ -5,15 +5,54 @@ const form = document.querySelector('.createTaskForm');
 
      const formData = new FormData(event.target);
      const task = formData.get('task');
-     addTask(task);
+     
+     if (task.trim()) {
+        addTask(task);
+        form.reset();
+     } 
+ }
 
-     form.reset();
+ const getUpdatedTemplate = (task) => {
+     return `
+        <div class="content">
+            <button class="checkbox"></button>
+            <input type="text" disabled value="${task}" class="taskInput"/>
+        </div>
+        <div class="actions">
+            <button class="star"></button>
+            <button class="edit"></button>
+            <button class="remove"></button>
+        </div>
+     `
+ }
+ const addRemoveHandler = (taskDom) => {
+     const removeDom = taskDom.querySelector('.remove');
+     removeDom.onclick = () => {
+         taskDom.remove();
+     }
+ }
 
+ const addCompleteHandler = (taskDom) => {
+     const checkboxDom = taskDom.querySelector('.checkbox');
+     checkboxDom.onclick = () => {
+         checkboxDom.classList.toggle('selected');
+         taskDom.classList.toggle('completed');
+     }
+
+ }
+ const addFavoriteHandler = (taskDom) =>{
+    const starDom = taskDom.querySelector('.star');
+    starDom.onclick = () => {
+        starDom.classList.toggle('selected');
+    }
  }
  const addTask = (task) => {
      const tasksDom = document.querySelector('.tasks');
      const taskDom = document.createElement('li');
 
-     taskDom.innerHTML = task;
+     taskDom.innerHTML = getUpdatedTemplate(task);
      tasksDom.prepend(taskDom);
+     addRemoveHandler(taskDom);
+     addCompleteHandler(taskDom);
+     addFavoriteHandler(tasksDom);
      }
