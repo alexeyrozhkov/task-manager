5278790d02641d6a3df769c3049716e7a75fb44a
arozhkov <alex31inq@gmail.com>
2020-09-16 20:57:46 +0300

    task

--- src/js/module.js
+++ src/js/module.js
@@ -1,3 +1,19 @@
-{
-    console.log('Hello Lectrum!');
-}
+
+const form = document.querySelector('.createTaskForm');
+ form.onsubmit = function(event) {
+     event.preventDefault();
+
+     const formData = new FormData(event.target);
+     const task = formData.get('task');
+     addTask(task);
+
+     form.reset();
+
+ }
+ const addTask = (task) => {
+     const tasksDom = document.querySelector('.tasks');
+     const taskDom = document.createElement('li');
+
+     taskDom.innerHTML = task;
+     tasksDom.prepend(taskDom);
+     }
